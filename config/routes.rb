ca7442a43897b3cc04d1fbcb9c3aa84512b35724
avinash singh <avinash8.as@gmail.com>
2023-07-24 19:55:09 -0400

    create logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login', to: "sessions#create"
-  # destroy '/logout', to:  
+  destroy '/logout', to: "sessions#destroy"  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
