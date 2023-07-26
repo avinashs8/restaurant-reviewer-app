4c575623c436c22f2b51f74f5eb4ca63d5e07adc
avinash singh <avinash8.as@gmail.com>
2023-07-26 11:22:58 -0400

    create login and signup components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../context/User';
 import NavBar from './NavBar';
 import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 
@@ -15,6 +17,8 @@ function App() {
       <NavBar/>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
       </Switch>
     </div>
   );
