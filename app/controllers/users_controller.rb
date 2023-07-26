ab46c2bcc77e8e67e3a7382a00c4ad139d1cb46d
avinash singh <avinash8.as@gmail.com>
2023-07-26 17:25:08 -0400

    add post for create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,23 @@ class UsersController < ApplicationController
 
     #signup
     def create 
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id 
+        else
 
+        end
+        render json: user
     end
 
     def show
         user = User.find(session[:id])
         render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
     end
 end
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from 'react';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
+import { UserContext } from '../context/User';
 
 function Signup() {
 
   const [ username, setUsername ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ passwordConfirm, setPasswordConfirm ] = useState('')
+  const [ errorList, setErrorList ] = useState([])
+  const { signup } = useContext(UserContext)
 
   const handleUserNameChange = e => {
     setUsername(e.target.value)
@@ -22,7 +25,29 @@ function Signup() {
 
   const handleSubmit = e => {
     e.preventDefault()
-
+    fetch('/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        passwordConfirmation: passwordConfirm
+      })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      if(!data.errors){
+        signup(data)
+      } else {
+        setUsername('')
+        setPassword('')
+        setPasswordConfirm('')
+        const errorLis = data.error.map(e => {
+          <li>{e}</li>
+        })
+        setErrorList(errorLis)
+      }
+    })
   }
 
   return (
@@ -33,11 +58,12 @@ function Signup() {
           <h5>Username:</h5>
           <TextField id="outlined-basic" label="Username" variant="outlined" autoFocus={true} value={username} onChange={handleUserNameChange}/>
           <h5>Password:</h5>
-          <TextField id="outlined-basic" label="Password" variant="outlined" value={password} onChange={handlePasswordChange}/>
+          <TextField id="outlined-basic" label="Password" type="password" variant="outlined" value={password} onChange={handlePasswordChange}/>
           <h5>Confirm Password:</h5>
-          <TextField id="outlined-basic" label="Password Confirmation" variant="outlined" value={passwordConfirm} onChange={handlePasswordConfirm}/>
-          <Button variant="contained" style={{ marginTop: '10px' }}>Contained</Button>
+          <TextField id="outlined-basic" label="Password Confirmation" type="password" variant="outlined" value={passwordConfirm} onChange={handlePasswordConfirm}/>
+          <Button variant="contained" style={{ marginTop: '10px' }}>Submit</Button>
         </form>
+        <ul>{errorList}</ul>
       </div>
     </div>
   );
