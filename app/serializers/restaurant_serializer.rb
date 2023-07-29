828967940a1601251874bce7cf3cdffe8ccfe5bd
avinash singh <avinash8.as@gmail.com>
2023-07-29 18:46:45 -0400

    create restaurant show page

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :cuisine, :price, :location
-  has_many :reviews
+  has_many :reviews 
+  has_many :users
 end
--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { useParams } from 'react-router-dom/cjs/react-router-dom'
+import ReviewCard from './ReviewCard'
+
 
 function Reviews({ restaurants }) {
     const { id } = useParams()
     const restaurant = restaurants.find(r => r.id === parseInt(id))
+    console.log(restaurant)
 
-  return (
+    const reviewsList = restaurant.reviews.map(review => {
+        return <ReviewCard key={review.id} review={review} restaurant={restaurant}/>
+    })
 
-    <div>{restaurant.name} Reviews</div>
-  )
+    console.log(reviewsList)
+    return (
+        <div className='centered' >
+            <div style={{ display: 'block' }}>
+                <h1>{restaurant.name} Reviews</h1>
+                <div>{reviewsList}</div>
+            </div>
+        </div>
+      );
+      
 }
 
 export default Reviews
