955338b7d0543624111805359786ce71a887cdd7
avinash singh <avinash8.as@gmail.com>
2023-07-29 17:50:37 -0400

    create reviews seed data

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom'
 
-function Reviews() {
+function Reviews({ restaurants }) {
+    const { id } = useParams()
+    const restaurant = restaurants.find(r => r.id === parseInt(id))
+
   return (
-    <div>Reviews</div>
+
+    <div>{restaurant.name} Reviews</div>
   )
 }
 
